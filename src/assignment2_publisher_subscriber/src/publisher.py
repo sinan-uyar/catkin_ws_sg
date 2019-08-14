c5e039dc87b18a3c8970961368efef1f8b422e37
sinan-uyar <sinan@sinanuyar.de>
2019-08-14 13:22:44 +0200

    publisher funktioniert nicht

--- src/assignment2_publisher_subscriber/src/publisher.py
+++ src/assignment2_publisher_subscriber/src/publisher.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
-# license removed for brevity
+
+
+# publisher according to talker chatter example from tutorial
+# publishes steer value 1.0 and speed value 0.3 m/s wit frequency of 10hz
 import rospy
+import autominy_msgs
+
 from autominy_msgs.msg import NormalizedSteeringCommand
 from autominy_msgs.msg import SpeedCommand
 
-def publisher():
-    pub = rospy.Publisher('/actuators/steering_normalized', NormalizedSteeringCommand, queue_size=10)
+def publi():
+    pub = rospy.Publisher('/actuators/steering', NormalizedSteeringCommand, queue_size=10)
     pub2 = rospy.Publisher('/actuators/speed', SpeedCommand, queue_size=10)
-    rospy.init_node('publisher', anonymous=True)
+    rospy.init_node('publi', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        pub.publish("args are", str(1.0))
-	pub2.publish("args are", str(0.3))
+        pub.publish(NormalizedSteeringCommand, 1.0)
+	pub2.publish(SpeedCommand, 0.3)
         rate.sleep()
 
 if __name__ == '__main__':
     try:
-        publisher()
+        publi()
     except rospy.ROSInterruptException:
         pass
 
