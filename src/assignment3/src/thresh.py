3f93a71e6ef5542a221cd96676a6d572cc272554
sinan-uyar <sinan@sinanuyar.de>
2019-08-15 17:14:15 +0200

    thresh.py

---
+++ src/assignment3/src/thresh.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import roslib
+#roslib.load_manifest('my_package')
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+  def __init__(self):
+    self.image_pub = rospy.Publisher("/sensors/camera/infra1/camera_info",Image)
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/sensors/camera/infra1/image_rect_raw",Image,self.callback)
+  def Threshold_Demo(val):
+    #0: Binary
+    #1: Binary Inverted
+    #2: Threshold Truncated
+    #3: Threshold to Zero
+    #4: Threshold to Zero Inverted
+    threshold_type = cv.getTrackbarPos(trackbar_type, window_name)
+    threshold_value = cv.getTrackbarPos(trackbar_value, window_name)
+    _, dst = cv.threshold(src_gray, threshold_value, max_binary_value, threshold_type )
+    cv.imshow(window_name, dst)
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    (rows,cols,channels) = cv_image.shape
+    #if cols > 60 and rows > 60 :
+    #  cv2.circle(cv_image, (50,50), 10, 255)
+    cv.threshold(cv_image, threshold_value, max_binary_value, threshold_type )
+    cv2.imshow("Image window", cv_image)
+    cv2.waitKey(3)
+
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
+
+def main():
+  rospy.init_node('image_converter', anonymous=True)
+  ic = image_converter()
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
