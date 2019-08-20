a7c5e89bcf4c99b11c2ef1d88a7cf660ab23f3d3
sinan-uyar <sinan@sinanuyar.de>
2019-08-20 16:07:45 +0200

    assignment 6 finished (new package needed)

--- src/assignment3/src/a6.py
+++ src/assignment3/src/a6.py
@@ -13,9 +13,9 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 def pabstand(gerade1, gerade2, punkt1):
-    abst=np.linalg.norm(np.cross(np.sub(punkt1, gerade1),gerade2))/np.linalg.norm(gerade2)
+    abst=np.linalg.norm(np.cross(np.subtract(punkt1, gerade1),gerade2))/np.linalg.norm(gerade2)
 
-    print(str(abst))
+    return(abst)
 
 class image_converter:
 
@@ -51,49 +51,76 @@ class image_converter:
 
     print(counter)
     '''
-    xy1=0
-    xy2=0
-    while xy1<200:
-        x1=random.randint(0,635)
-        y1=random.randint(0,475)
-        xy1=cv_image[y1,x1]
-
-    while xy2<200:
-        x2=random.randint(0,635)
-        y2=random.randint(0,475)
-        xy2=cv_image[y2,x2]
-    '''
-    m=(y2-y1)*1.0 / (x1+x2)
-    n=(-1*x1)+y1
-    mbest=m
-    nbest=n
     
-    print("f(x)= "+str(m)+"*x+"+str(n))
 
-    #print(m, y1,y2,x1,x2)
-    '''
-    '''
-    for x in range(0,635):
-        ye= int(abs(x*m+n))
-        #xe=int(xe)
-        #if cv_image[y,xe] >=200:
-        #    counter+=1
-        print(x,ye)
-    #if counter>(475*0.5):
-    n2=n+10
-    n3=n-10
-    '''
-    
-    
-    for y in range(0,479):
-        for x in range(0,639):
-    	    px=cv_image[y,x]
-            if px>=200:
-                pabst(np.array([x1,y1]),np.array([x2-x1,y2-y1]),np.array([x,y]))
-            x=x+1
-        y=y+1
+    x7=0
+    while x7<3:
+	    x6=0
+	    x1best=0
+	    y1best=0
+	    x2best=0
+	    y2best=0
+	    bestcounter=0
+	    while x6<8:
+		    xy1=0
+		    xy2=0
+		    while xy1<200:
+			x1=random.randint(0,635)
+			y1=random.randint(0,475)
+			xy1=cv_image[y1,x1]
 
-    cv2.line(cv_image,(x1,y1),(x2,y2),(255,255,255),20)
+		    while xy2<200:
+			x2=random.randint(0,635)
+			y2=random.randint(0,475)
+			xy2=cv_image[y2,x2]
+		    '''
+		    m=(y2-y1)*1.0 / (x1+x2)
+		    n=(-1*x1)+y1
+		    mbest=m
+		    nbest=n
+		    
+		    print("f(x)= "+str(m)+"*x+"+str(n))
+
+		    #print(m, y1,y2,x1,x2)
+		    '''
+		    '''
+		    for x in range(0,635):
+			ye= int(abs(x*m+n))
+			#xe=int(xe)
+			#if cv_image[y,xe] >=200:
+			#    counter+=1
+			print(x,ye)
+		    #if counter>(475*0.5):
+		    n2=n+10
+		    n3=n-10
+		    '''
+		    counter=0
+		    for y in range(0,479):
+			for x in range(0,639):
+		    	    px=cv_image[y,x]
+		            x4=x
+		            y4=y
+			    if px>=200:
+				erg=pabstand(np.array([x1,y1]),np.array([x2-x1,y2-y1]),np.array([x4,y4]))
+				if erg<5:
+		                    #cv2.line( cv_image,(x,y),(x,y),(0,0,0) )
+				    counter+=1
+				    #print(x,y,erg)
+			    x=x+1
+			y=y+1
+		    x6+=1
+		    if counter>bestcounter:
+		        bestcounter=counter
+		        x1best=x1
+			y1best=y1
+			x2best=x2
+			y2best=y2
+	    
+	    m=(y2best-y1best)*1.0 / (x2best-x1best)
+	    n=y1best-m*x1best
+	    
+	    cv2.line(cv_image,(0,int(n)),(640,int(m*640+n)),(100,100,100),30)
+            x7+=1
 
     ret,th1=cv2.threshold(cv_image, 200, 255, cv2.THRESH_BINARY )
 
